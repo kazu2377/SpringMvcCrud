0eec8e918b0ae7755499f1d42a678b91ceb8a72c
kazu2377 <60035711+kazu2377@users.noreply.github.com>
2023-01-09 17:33:39 +0900

    SpringMvcCrudのサンプル
    https://www.youtube.com/watch?v=4cEmPxJuP40&list=PLOiid1_Qchpi6POvS0E30YW6Wn0FU8IJe&index=2

---
+++ SpringMvcCrud/src/main/java/com/demo/MainController.java
@@ -0,0 +1,14 @@
+package com.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+    
+    @RequestMapping ("test")
+    public String viewHome () {
+        return "home";
+    }
+    
+}
